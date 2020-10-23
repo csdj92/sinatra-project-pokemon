4f49772a800820588a9615a9022b3629030853ea
csdj92 <csdj92@gmail.com>
2020-10-23 12:16:23 -0400

    Make pokemon cont

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,3 +1,14 @@
 class PokemonController < ApplicationController
+
+    get "/pokemon" do 
+        authenticate
+        @pokemon = Pokemon.all
+        erb :'pokemon/index'
+    end
+
+    get "/pokemon/new" do 
+        authenticate
+        erb :'pokemon/new'
+    end
     
 end
