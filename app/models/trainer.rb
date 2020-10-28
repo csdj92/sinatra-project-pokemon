6878f9c61e9be327b6cfcb346266d4d32a58920a
csdj92 <csdj92@gmail.com>
2020-10-28 14:44:54 -0400

    Refactored model

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,7 +1,6 @@
 class Trainer < ActiveRecord::Base
     has_secure_password   
     has_many :pokemon
-    #has_many :types, through: :pokemon
     validates :trainer_name, :email, :password, presence: true
     validates :password, length: { minimum: 5 }, confirmation: true, 
       unless: Proc.new { |t| t.password.blank? }
